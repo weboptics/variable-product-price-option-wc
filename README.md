0b7a423fa5205e261d71d8052e9f3728b0b3eb33
Haseeb Nawaz <haseebnawaz298@gmail.com>
2024-04-21 14:41:56 +0500

    added enable disable feature

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Contributors: haseebnawaz298
 Tags: woocommerce, variable price, product price, change price,custom price,
 Requires at least: 4.5.0
 Tested up to: 6.4.3
-Stable tag: 1.0.1
+Stable tag: 1.0.2
 WC requires at least: 3
 WC tested up to: 5.6.0
 License: GPL v2 or later
@@ -17,8 +17,35 @@ This plugin gives the ability to alter price of product in wordpress Woocommerce
 
 Variable Product Price Option for WooCommerce" is a powerful WordPress plugin designed to enhance your WooCommerce store's flexibility and customer experience. With this plugin, you can offer dynamic pricing options for your products
 
+
+= Features =
+
+* Lets you enable custom product price for any product type.
+* Compatible with many themes.
+* Compatible with Easy to use.
+
+== Frequently Asked Questions ==
+
+= I need help to configure the plugin? =
+
+Please email me at <a href="mailto:haseebnawaz298@gmail.com" target="_blank">haseebnawaz298@gmail.com</a>
+
+= For support or troubleshooting help =
+
+Submit a issue <a href="https://github.com/webzombies/variable-product-price-option-wc/issues">here</a>.
+
+== Installation ==
+
+1. Download the plugin & install it to your `wp-content/plugins` folder (or use the Plugins menu through the WordPress Administration section)
+2. Activate the plugin
+3. Navigate to **WooCommerce > Edit Product**.
+4. Select your option.
+5. Save and enjoy!
+
+
 == Screenshots ==
 1. change the price by clicking on the custom button and adding price
+2. you can enable the option by clicking on **Enable Custom Price Checkbox**
 
 == Changelog ==
 
@@ -27,3 +54,9 @@ Variable Product Price Option for WooCommerce" is a powerful WordPress plugin de
 
 = 1.0.1 =
 * Add banner, icon and screenshot
+
+= 1.0.2 =
+
+* Option to enable disable custom price feature.
+* added install instruction
+* added screenshot
--- variable-product-price-option-wc.php
+++ variable-product-price-option-wc.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Variable Product Price Option for WooCommerce
  * Plugin URI:        https://github.com/webzombies/variable-product-price-option-wc
  * Description:       This plugin gives the ability to alter price of product in wordpress Woocommerce.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Requires at least: 5.8
  * Requires PHP:      7.2
  * Author:            Haseeb Nawaz Awan
@@ -31,13 +31,22 @@ class HS_WCVPO_Init{
 		add_filter( 'woocommerce_cart_item_price', array($this,'filter_cart_item_price'), 10, 3 );
 		add_filter( 'woocommerce_cart_item_subtotal', array($this,'show_product_discount_order_summary'), 10, 3 );
 		add_filter( 'woocommerce_add_to_cart_redirect', array($this,'custom_add_to_cart_redirect'), 10, 2 );
+
+		add_action('woocommerce_product_options_general_product_data', array($this,'add_custom_checkbox_field'));
+		add_action('woocommerce_process_product_meta', array($this,'save_custom_checkbox_field'));
+		add_action('woocommerce_after_single_product_summary', array($this,'display_custom_checkbox_field'), 5);
 	}
 	function single_variation_callback(){
-		$jq="jQuery('#variable-price').toggle();";
-		echo '<div class="wc-price-custom" style="padding-bottom: 5px;">';
+		global $product;
+		$id = $product->get_id();
+		$enable_custom_price = get_post_meta($id, '_enable_custom_price', true);
+		if('yes'===$enable_custom_price){
+			$jq="jQuery('#variable-price').toggle();";
+			echo '<div class="wc-price-custom" style="padding-bottom: 5px;">';
 			echo '<a type="button" class="custom-button" onclick="'.esc_html( $jq ).'">Custom</a>';
 			echo '<div style="display:none;" id="variable-price"><div class="form-group"><input class="woocommerce-Input woocommerce-Input--text input-text" type="number" name="custom-price" value="0" id="custom-price"></div></div>';
-		echo '</div>';
+			echo '</div>';
+		}
 	}
 
 	function change_cart_item_data($cart_item_data, $product_id, $variation_id){
@@ -101,5 +110,45 @@ class HS_WCVPO_Init{
 		}
 		return $url;
 	}
+
+	// Add custom checkbox field to product
+	function add_custom_checkbox_field() {
+		global $product;
+
+		echo '<div class="custom-field">';
+		woocommerce_wp_checkbox(
+			array(
+				'id' => '_enable_custom_price',
+				'label' => __('Enable Custom Price', 'woocommerce'),
+				'desc_tip' => 'true',
+				'description' => __('Check this box to enable a custom price for this product.', 'woocommerce')
+			)
+		);
+		echo '</div>';
+	}
+
+
+	// Save custom checkbox field data
+	function save_custom_checkbox_field($product_id) {
+		$enable_custom_price = isset($_POST['_enable_custom_price']) ? 'yes' : 'no';
+		update_post_meta($product_id, '_enable_custom_price', $enable_custom_price);
+	}
+
+
+	// Display custom checkbox field value on product page
+	function display_custom_checkbox_field() {
+		global $post;
+
+		$enable_custom_price = get_post_meta($post->ID, '_enable_custom_price', true);
+
+		if ($enable_custom_price === 'yes') {
+			echo '<div class="custom-field-value">';
+			echo '<strong>' . __('Custom Price Enabled:', 'woocommerce') . '</strong> ' . __('Yes', 'woocommerce');
+			echo '</div>';
+		}
+	}
+
+
 }
+
 new HS_WCVPO_Init();
